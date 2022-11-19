f33d0cb39aa481ceb969d1d84db0742e2d3675e0
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2022-11-19 17:50:43 +0200

    Initial commit

---
+++ src/hw1/task1/Rectangle.java
@@ -0,0 +1,12 @@
+//Використовуючи IDEA, створіть проект із пакетом.
+// Потрібно: Створити клас із ім'ям Rectangle. У тілі класу створити два поля,
+// що описують довжини сторін double side1, double side2.
+// Створити два методи, що обчислюють площу прямокутника - double areaCalculator
+// (double side1, double side2) і периметр прямокутника - double perimeterCalculator
+// (double side1, double side2). Написати програму, яка приймає від користувача довжини
+// двох сторін прямокутника і виводить на екран периметр та площу.
+
+
+package hw1.task1;
+public class Rectangle {
+}
