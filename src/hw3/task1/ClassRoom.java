eba1748bbc34218436cf873836c4615723ebd9dc
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2022-11-26 21:57:31 +0200

    Initial commit

--- src/hw3/task1/ClassRoom.java
+++ src/hw3/task1/ClassRoom.java
@@ -13,11 +13,25 @@ class ClassRoom {
     Pupil excellentPupil = new ExcellentPupil();
     Pupil goodPupil = new GoodPupil();
     Pupil badPupil = new BadPupil();
+    Pupil pupil = new Pupil();
 
-    public ClassRoom(Pupil excellentPupil, Pupil goodPupil, Pupil badPupil) {
+    ClassRoom(Pupil excellentPupil, Pupil goodPupil, Pupil badPupil) {
         this.excellentPupil = excellentPupil;
         this.goodPupil = goodPupil;
         this.badPupil = badPupil;
+    }
+
+
+    ClassRoom(Pupil excellentPupil, Pupil goodPupil, Pupil badPupil, Pupil pupil) {
+        this.excellentPupil = excellentPupil;
+        this.goodPupil = goodPupil;
+        this.badPupil = badPupil;
+        this.pupil = pupil;
+    }
+
+    ClassRoom(Pupil excellentPupil, Pupil goodPupil) {
+        this.excellentPupil = excellentPupil;
+        this.goodPupil = goodPupil;
     }
 
     public ClassRoom() {
@@ -25,25 +39,33 @@ class ClassRoom {
     }
 
     public static void main(String[] args) {
-    ClassRoom classRoom = new ClassRoom();
+        ClassRoom classRoom = new ClassRoom();
         System.out.println("----------------------------");
-        System.out.println("----Bad Pupil: ");
-        classRoom.badPupil.read();
-        classRoom.badPupil.relax();
-        classRoom.badPupil.study();
-        classRoom.badPupil.write();
-        System.out.println("----------------------------");
-        System.out.println("----Good pupil: ");
-        classRoom.goodPupil.write();
-        classRoom.goodPupil.study();
-        classRoom.goodPupil.read();
-        classRoom.goodPupil.relax();
-        System.out.println("----------------------------");
+        System.out.println("----ClassRoom 1------");
+        ClassRoom classRoom1 = new ClassRoom(classRoom.excellentPupil, classRoom.goodPupil, classRoom.goodPupil, classRoom.badPupil);
         System.out.println("----Excellent pupil");
-        classRoom.excellentPupil.relax();
-        classRoom.excellentPupil.read();
-        classRoom.excellentPupil.study();
-        classRoom.excellentPupil.write();
+        classRoom1.excellentPupil.relax();
+        classRoom1.excellentPupil.read();
+        classRoom1.excellentPupil.study();
+        classRoom1.excellentPupil.write();
+        System.out.println("----Good pupil: ");
+        classRoom1.goodPupil.write();
+        classRoom1.goodPupil.study();
+        classRoom1.goodPupil.read();
+        classRoom1.goodPupil.relax();
+        System.out.println("----Bad Pupil: ");
+        classRoom1.badPupil.read();
+        classRoom1.badPupil.relax();
+        classRoom1.badPupil.study();
+        classRoom1.badPupil.write();
         System.out.println("----------------------------");
+        System.out.println("----ClassRoom 2------");
+        ClassRoom classRoom2 = new ClassRoom(classRoom.excellentPupil, classRoom.excellentPupil);
+        System.out.println("----Excellent pupil");
+        classRoom2.excellentPupil.relax();
+        classRoom2.excellentPupil.read();
+        classRoom2.excellentPupil.study();
+        classRoom2.excellentPupil.write();
+
     }
 }
