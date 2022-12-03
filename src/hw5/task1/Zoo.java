1e6ea8eefdf8f881eff0f514225bbe40dd7b6b06
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2022-12-03 16:39:41 +0200

    Initial commit

--- src/hw5/task1/Zoo.java
+++ src/hw5/task1/Zoo.java
@@ -4,21 +4,20 @@ import java.util.ArrayList;
 
 class Zoo {
     public static void main(String[] args) {
-        ArrayList <String> zoo = new ArrayList<>();
-        zoo.add(0,"Lion");
+        ArrayList<String> zoo = new ArrayList<>();
+        zoo.add(0, "Lion");
         zoo.add(1, "Wolf");
         zoo.add(2, "Bear");
         zoo.add(3, "Fox");
-        zoo.add(4,"Giraffe");
-        zoo.add(5,"Koala");
+        zoo.add(4, "Giraffe");
+        zoo.add(5, "Koala");
         zoo.add(6, "Strauss");
         zoo.add(7, "Tiger");
         System.out.println("--------------------");
         System.out.println(zoo);
         System.out.println("--------------------");
-        for (String zo: zoo) {
+        for (String zo : zoo) {
             System.out.println(zo + " ");
         }
-        System.out.println("--------------------");
     }
 }
